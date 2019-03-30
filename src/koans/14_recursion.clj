5869d33556e6097edaf1093e69080fd5add054fe
rodrigo-kondo <rodi.kondo@gmail.com>
2019-03-29 22:40:03 -0300

    Completed with no bugs...

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,28 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (reduce conj '() coll) )
 
 (defn factorial [n]
-  __)
+  (loop [ x n f 1]
+    (if (= x 1)
+      f
+      (recur (dec x) (* f x)))))
+
+  
+     
+    
 
 (meditations
   "Recursion ends with a base case"
